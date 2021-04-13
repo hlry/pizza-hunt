c9f8852c761e000b8a52b4f0729efac7ba0ad8ce
Hilary Richardson <hilary.richardson@gmail.com>
2021-04-12 20:50:46 -0700

    creating comment models, routes, and fixing date UI

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,5 +1,7 @@
 const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
+//schema
 const PizzaSchema = new Schema({
     pizzaName: {
         type: String
@@ -9,13 +11,33 @@ const PizzaSchema = new Schema({
     },
     createdAt: {
         type: Date,
-        default: Date.now
+        default: Date.now,
+        get: (createdAtVal) => dateFormat(createdAtVal)
     },
     size: {
         type: String,
         default: 'Large'
     },
-    toppings: []
+    toppings: [],
+    comments: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Comment'
+        }
+    ]
+},
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+// get total count of comments and replies on retrieval
+PizzaSchema.virtual('commentCount').get(function () {
+    return this.comments.length;
 });
 
 // create the Pizza model using the PizzaSchema
